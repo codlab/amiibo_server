d254da88d61be36b566879655a52028f89127720
codlab <codlabtech@gmail.com>
2015-11-01 15:28:01 +0100

    fix typo

--- db/migrate/20151101141849_test.rb
+++
@@ -1,9 +0,0 @@
-class Test < ActiveRecord::Migration
-  def up
-execute "insert into revisions (revision, created_at, updated_at) values (1, '#{Time.now}', '#{Ti
-me.now}')"
-  end
-
-  def down
-  end
-end
