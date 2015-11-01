e7e2e1d3b87d8905f32fe2fe79394356cff83a59
codlab <codlabtech@gmail.com>
2015-11-01 17:05:40 +0100

    add methods into revs

--- app/seeds/seed_content.rb
+++ app/seeds/seed_content.rb
@@ -1,10 +1,25 @@
+def insert_if_not_exists(connection, name, identifier)
+  count = connection.execute "select count(*) from amiibo_ids where identifier = '#{identifier}'"
+  count = count.to_a
+  Rails.logger.info("count #{count}")
+  if count == 0
+    connection.execute "insert into amiibo_ids (identifier, name, created_at, updated_at) values ('#{identifier}', '#{name}', '#{Time.now}', '#{Time.now}')"
+  end
+end
+
+def insert_if_not_exists_revision(connection, revision)
+  count = connection.execute "select count(*) from revisions where revision = #{revision}"
+  count = count.to_a
+  Rails.logger.info("count revision #{count}")
+  if count == 0
+    connection.execute "insert into revisions (revision, created_at, updated_at) values (#{revision}, '#{Time.now}', '#{Time.now}')"
+  end
+end
+
 connection = ActiveRecord::Base.connection
 
+insert_if_not_exists_revision(connection, 1)
+insert_if_not_exists_revision(connection, 2)
 
-connection.execute "insert into revisions (revision, created_at, updated_at) values (1, '#{Time.now}', '#{Time.now}')"
-
-connection.execute "insert into revisions (revision, created_at, updated_at) values (2, '#{Time.now}', '#{Time.now}')"
-
-
-connection.execute "insert into amiibo_ids (identifier, name, created_at, updated_at) values ('000a000000380102', 'Toad', '#{Time.now}', '#{Time.now}')"
-connection.execute "insert into revisions (revision, created_at, updated_at) values (3, '#{Time.now}', '#{Time.now}')"
+insert_if_not_exists("Toad", "000a000000380102")
+insert_if_not_exists_revision(connection, 3)
