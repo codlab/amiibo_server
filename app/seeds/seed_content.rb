63daaedbad758a5db8df72a6eff3f4b0a43aae21
codlab <codlabtech@gmail.com>
2015-11-01 16:18:56 +0100

    fix issue with rev

--- app/seeds/seed_content.rb
+++ app/seeds/seed_content.rb
@@ -5,5 +5,6 @@ connection.execute "insert into revisions (revision, created_at, updated_at) val
 
 connection.execute "insert into revisions (revision, created_at, updated_at) values (2, '#{Time.now}', '#{Time.now}')"
 
-connection.execute "insert into amiibo_ids (identifier, name) values ('000a000000380102', "Toad")"
+
+connection.execute "insert into amiibo_ids (identifier, name) values ('000a000000380102', 'Toad')"
 connection.execute "insert into revisions (revision, created_at, updated_at) values (3, '#{Time.now}', '#{Time.now}')"
