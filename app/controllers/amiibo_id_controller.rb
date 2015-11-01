0fe055a209a47551c4a7645fdf0ecf56400e51bc
codlab <codlabtech@gmail.com>
2015-11-01 23:48:37 +0100

    update render

--- app/controllers/amiibo_id_controller.rb
+++ app/controllers/amiibo_id_controller.rb
@@ -2,8 +2,13 @@ class AmiiboIdController < ApplicationController
   def list
     hash = Hash.new
     hash[:revision] = Revision.order("revision DESC ").first.revision
-    hash[:amiibos] = AmiiboId.all
     hash[:apk] = { :version => 2, :url => "http://amiibo.codlab.eu/amiibo_2.apk"}
+    amiibos = []
+    amiibos_db = AmiiboId.all
+    amiibos_db.each do |amiibo|
+        amiibos << {:name => amiibo.name, :identifier => amiibo.identifier}
+    end
+    hash[:amiibos] = amiibos
 
     respond_with hash
   end
