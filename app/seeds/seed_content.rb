d8cf39aa3bbf80c735158fd41122018431c65fb6
codlab <codlabtech@gmail.com>
2015-11-01 16:14:17 +0100

    update revision to 3

--- app/seeds/seed_content.rb
+++ app/seeds/seed_content.rb
@@ -1,5 +1,9 @@
 connection = ActiveRecord::Base.connection
+
+
 connection.execute "insert into revisions (revision, created_at, updated_at) values (1, '#{Time.now}', '#{Time.now}')"
 
-connection = ActiveRecord::Base.connection
 connection.execute "insert into revisions (revision, created_at, updated_at) values (2, '#{Time.now}', '#{Time.now}')"
+
+connection.execute "insert into amiibo_ids (identifier, name) values ("000a000000380102", "Toad")"
+connection.execute "insert into revisions (revision, created_at, updated_at) values (3, '#{Time.now}', '#{Time.now}')"
--- app/seeds/seeds.rb
+++ app/seeds/seeds.rb
@@ -1,8 +1,9 @@
-current_revision = 2
+current_revision = 3
 
 if Revision.count == 0 or Revision.order("revision DESC ").first.revision < current_revision
   connection = ActiveRecord::Base.connection
   connection.execute "TRUNCATE revisions RESTART IDENTITY"
+  connection.execute "TRUNCATE amiibo_ids RESTART IDENTITY"
 
   load(Rails.root.join("app", "seeds", "seed_content.rb"))
 end
