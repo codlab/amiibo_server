877e0d1482caebda324ccb22245d07e91565296d
codlab <codlabtech@gmail.com>
2015-11-01 16:45:42 +0100

    add created_at

--- app/seeds/seed_content.rb
+++ app/seeds/seed_content.rb
@@ -6,5 +6,5 @@ connection.execute "insert into revisions (revision, created_at, updated_at) val
 connection.execute "insert into revisions (revision, created_at, updated_at) values (2, '#{Time.now}', '#{Time.now}')"
 
 
-connection.execute "insert into amiibo_ids (identifier, name, created_at) values ('000a000000380102', 'Toad', '#{Time.now}')"
+connection.execute "insert into amiibo_ids (identifier, name, created_at, updated_at) values ('000a000000380102', 'Toad', '#{Time.now}', '#{Time.now}')"
 connection.execute "insert into revisions (revision, created_at, updated_at) values (3, '#{Time.now}', '#{Time.now}')"
