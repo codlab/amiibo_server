dfb04fb55a08d3f8963bf3e69676def41f28a250
codlab <codlabtech@gmail.com>
2015-11-01 15:13:20 +0100

    add secret token, :wink: :wink:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amiibo::Application.config.secret_token = '3f34dcbca0d365d1df8a350ee20d2ac493dc5a9a9129902479f5746f7ec5bc89176f55afe0dfe3a1501b5bd1003a297c1a22b395a0c8bd332beb48d78047f02b'
