a26ce76bbe28966897ddd998db8a0fa5aae9fa7c
codlab <codlabtech@gmail.com>
2015-11-01 15:59:33 +0100

    update issue with seed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
   def check_seed
     logger.info("having revision count #{Revision.count}")
     if Revision.count < 1
-      load(Rails.root.join("db", "seeds.rb"))
+      load(Rails.root.join("app", "seeds", "seeds.rb"))
     end
   end
 end
